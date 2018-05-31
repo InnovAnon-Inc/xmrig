a36e3b379da410e02c5a8fcdba97b0e9b5d059ab
Kai Killer <laurence.a.maddox@gmail.com>
2018-05-30 22:27:15 -0500

    update

--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@ set -exo nounset
 
 sudo nice -n -20 \
 	build/xmrig --donate-level=0 \
-	-u 43To46Y9AxNFkY5rsMQaLwbRNaxLZVvc4LJZt7Cx9Dt23frL6aut2uC3PsMiwGY5C5fKLSn6sWyoxRQTK1dhdBpKAX8bsUW \
-	-p work01 -o us-backup.supportxmr.com
+	--config=./laptop.json
